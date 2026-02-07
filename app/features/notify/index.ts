07101fbdabafff7a6bb7bdf1a5a4ba4370b93c92
darthrommy <88261399+darthrommy@users.noreply.github.com>
2026-02-07 22:37:08 +0900

    Stop triggering CRON jobs Fixes #40

--- app/features/notify/index.ts
+++ app/features/notify/index.ts
@@ -2,4 +2,3 @@ export { cancelNotification } from "./cancel-notification.server";
 export * from "./cookie.server";
 export { getNotification } from "./get-notification.server";
 export { registerNotifiaction } from "./register-notification.server";
-export { sendNotification } from "./send-notification.server";
